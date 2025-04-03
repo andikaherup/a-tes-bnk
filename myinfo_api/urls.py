7c10cda0bd7d88194a39fde026c3c395b1ea5cc4
Andika Heru P <andikaherup@gmail.com>
2025-04-03 09:42:48 +0700

    update urls

--- myinfo_api/urls.py
+++ myinfo_api/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import MyInfoAuthorizeView, MyInfoCallbackView, MyInfoDataView
+from .views import (
+    MyInfoAuthorizeView, 
+    MyInfoCallbackView, 
+    MyInfoDataView,
+    MyInfoProfileView,
+    MyInfoStatusView,
+    MyInfoLogoutView
+)
 
 urlpatterns = [
     path('authorize/', MyInfoAuthorizeView.as_view(), name='myinfo-authorize'),
     path('callback/', MyInfoCallbackView.as_view(), name='myinfo-callback'),
     path('data/', MyInfoDataView.as_view(), name='myinfo-data'),
+    path('profile/', MyInfoProfileView.as_view(), name='myinfo-profile'),
+    path('status/', MyInfoStatusView.as_view(), name='myinfo-status'),
+    path('logout/', MyInfoLogoutView.as_view(), name='myinfo-logout'),
 ]
