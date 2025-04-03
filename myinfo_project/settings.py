fb1e4cda2c3347eb0a6a420f989466fff6962d1f
Andika Heru P <andikaherup@gmail.com>
2025-04-03 09:33:18 +0700

    add myinfo middleware to settings

--- myinfo_project/settings.py
+++ myinfo_project/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'myinfo_api.middleware.MyInfoExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'myinfo_project.urls'
