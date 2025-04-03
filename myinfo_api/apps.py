6e924052b9f422dee659400f425ae46bf79ea6e5
Andika Heru P <andikaherup@gmail.com>
2025-04-03 09:43:19 +0700

    create app file for myinfo api

--- myinfo_api/apps.py
+++ myinfo_api/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
 
 
-class MyinfoApiConfig(AppConfig):
+class MyInfoApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myinfo_api'
+    verbose_name = 'MyInfo API Integration'
+
+    def ready(self):
+        """
+        Perform initialization tasks when the app is ready.
+        """
+        # Import any signal handlers if needed
+        # from . import signals
+        pass
